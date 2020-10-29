88f0f0ea520b852d85c63c86eb5b13c068063310
Thiago Sudré Cezarino <thiagoty@gmail.com>
2020-10-29 00:17:56 -0300

    [ADD] Tipagem dos parametros

--- models.py
+++ models.py
@@ -2,46 +2,46 @@ from random import randint
 
 class City:
     def __init__(self):
-        self.name = ''
-        self.id = -1
-        self.distance = []
+        self.name: str = ''
+        self.id: int = -1
+        self.distance: list = []
 
     def __repr__(self):
         return str(self.id)
     
-    def set_name(self, name):
+    def set_name(self, name: str):
         self.name = name
 
-    def set_id(self, id):
+    def set_id(self, id: int):
         self.id = id
 
-    def add_distance(self, distance):
+    def add_distance(self, distance: int):
         self.distance.append(distance)
     
 class CityGroup:
     def __init__(self):
-        self.array_city = []
+        self.array_city:list = []
     
-    def add_city(self, newCity):
+    def add_city(self, newCity: City):
         self.array_city.append(newCity)
         newCity.set_id(len(self.array_city) - 1)
 
-    def remove_city(self, city):
+    def remove_city(self, city: City):
         self.array_city.append(city)
     
     def get_cities(self):
         return self.array_city
 
 class Generator:
-    def __init__(self, size):
+    def __init__(self, size: int):
         self.cities = CityGroup()
-        self.size = size
+        self.size: int = size
 
     def generate_cities(self):
         for i in range(self.size):
             self.cities.add_city(City())
 
-    def generate_distances(self, max_distance):
+    def generate_distances(self, max_distance: int):
         cities = self.cities.get_cities()
 
         # percorre cada cidade colocando uma distancia aleatoria entre as cidades
@@ -54,14 +54,14 @@ class Generator:
                     cities[i].add_distance(distance)
                     cities[j].add_distance(distance)
     
-    def get_cities(self):
+    def get_cities(self): 
         return self.cities
 
 class PopulationGenerator:
-    def __init__(self, cities, population_size):
+    def __init__(self, cities: CityGroup, population_size: int):
         self.cities = cities.get_cities()
         self.population_size = population_size
-        self.population = []
+        self.population: list = []
 
     def generate_populations(self):
         for i in range(self.population_size):
@@ -79,11 +79,11 @@ class PopulationGenerator:
         return self.population
 
 class FitnessCalculator:
-    def __init__(self, population):
+    def __init__(self, population: list):
         self.population = population
-        self.distances = []
-        self.fitness = []
-        self.distance_sum = 0
+        self.distances: list = []
+        self.fitness: list = []
+        self.distance_sum: int = 0
     
     def calulate_total_distances(self):
         for individual in self.population:
